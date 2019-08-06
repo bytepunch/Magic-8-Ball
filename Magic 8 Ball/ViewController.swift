4032ac6afa52498a98434fbfc8de51d0a9e8e6dc
Jörg <bytepunch@gmail.com>
2019-08-06 13:53:54 +0200

    Visual elements placed

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var imageViewBall: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func askMe(_ sender: Any) {
+        
+        
+        
+        
+        
+    }
+    
 
 
 }
