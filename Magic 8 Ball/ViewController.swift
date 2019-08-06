8e4b9ca7f1d610d6ade5b955d1e972b165dd6c63
Jörg <bytepunch@gmail.com>
2019-08-06 14:22:19 +0200

    Some functionality so that each function call gives another image and not the same again

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     let ballNames = ["ball1", "ball2", "ball3", "ball4", "ball5"]
     var randomValue : Int = 0
     
+    var tempRandomValue: Int = 999
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +39,12 @@ class ViewController: UIViewController {
     
     func setBallByRandom(){
         randomValue = Int.random(in: 0 ... ballNames.count - 1)
+        while tempRandomValue == randomValue {
+            randomValue = Int.random(in: 0 ... ballNames.count - 1)
+        }
         imageViewBall.image = UIImage(named: ballNames[randomValue])
+        tempRandomValue = randomValue
+        
     }
 }
 
